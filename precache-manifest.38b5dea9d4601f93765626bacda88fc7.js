3a4a2575dbdf732eb49bfad32a7e4264e5511b72
Gustavo Matsuo <GustavoMatsuoF@gmail.com>
2019-09-20 18:01:34 -0300

    Updates

---
+++ precache-manifest.38b5dea9d4601f93765626bacda88fc7.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b135b801c799fa62882f9a616bb95da3",
+    "url": "/Website/index.html"
+  },
+  {
+    "revision": "f08b2499fcd70009a259",
+    "url": "/Website/static/css/main.54ec06ff.chunk.css"
+  },
+  {
+    "revision": "8b73a02fc108c0cd6175",
+    "url": "/Website/static/js/2.8c2c6619.chunk.js"
+  },
+  {
+    "revision": "f08b2499fcd70009a259",
+    "url": "/Website/static/js/main.9845e954.chunk.js"
+  },
+  {
+    "revision": "efc80d5f5b3e8af147a6",
+    "url": "/Website/static/js/runtime-main.8f32a843.js"
+  },
+  {
+    "revision": "dc4a71bae8b561f542b3d36b9129f33d",
+    "url": "/Website/static/media/github-1.dc4a71ba.svg"
+  },
+  {
+    "revision": "930ab2cc72376b3f9a418c47079fee8b",
+    "url": "/Website/static/media/instagram-11.930ab2cc.svg"
+  },
+  {
+    "revision": "ac9ddb2337691036ac098ed774cf48ca",
+    "url": "/Website/static/media/linkedin-3.ac9ddb23.svg"
+  },
+  {
+    "revision": "c8bfb9dcb5e6763a7aa5f5eaa621f2b4",
+    "url": "/Website/static/media/perfil.c8bfb9dc.jpg"
+  }
+]);
